eab663d6f1b271a9c55fda6f80adedcc36dbf4e9
lkdjfalnnlvz <155760188+lkdjfalnnlvz@users.noreply.github.com>
2025-10-18 17:37:11 +0900

    Refactor server color assignment and logging logic

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
-const App = require('./lib/app')
-const ServerRegistration = require('./lib/servers')
+const App = require('./lib/app');
+const ServerRegistration = require('./lib/servers');
+const logger = require('./lib/logger');
+const config = require('./config');
+const servers = require('./servers');
 
-const logger = require('./lib/logger')
-
-const config = require('./config')
-const servers = require('./servers')
-
-const app = new App()
+const app = new App();
 
 servers.forEach((server, serverId) => {
-  // Assign a generated color for each servers.json entry if not manually defined
-  // These will be passed to the frontend for use in rendering
   if (!server.color) {
-    let hash = 0
+    let hash = 0;
     for (let i = server.name.length - 1; i >= 0; i--) {
-      hash = server.name.charCodeAt(i) + ((hash << 5) - hash)
+      hash = server.name.charCodeAt(i) + ((hash << 5) - hash);
     }
-
-    const color = Math.floor(Math.abs((Math.sin(hash) * 10000) % 1 * 16777216)).toString(16)
-    server.color = '#' + Array(6 - color.length + 1).join('0') + color
+    const color = Math.floor(Math.abs((Math.sin(hash) * 10000) % 1 * 16777216)).toString(16);
+    server.color = '#' + Array(6 - color.length + 1).join('0') + color;
   }
-
-  // Init a ServerRegistration instance of each entry in servers.json
-  app.serverRegistrations.push(new ServerRegistration(app, serverId, server))
-})
+  app.serverRegistrations.push(new ServerRegistration(app, serverId, server));
+});
 
 if (!config.serverGraphDuration) {
-  logger.log('warn', '"serverGraphDuration" is not defined in config.json - defaulting to 3 minutes!')
-  config.serverGraphDuration = 3 * 60 * 10000
+  logger.log('warn', '"serverGraphDuration" is not defined in config.json - defaulting to 3 minutes!');
+  config.serverGraphDuration = 3 * 60 * 10000;
 }
 
-if (!config.logToDatabase) {
-  logger.log('warn', 'Database logging is not enabled. You can enable it by setting "logToDatabase" to true in config.json. This requires sqlite3 to be installed.')
-
-  app.handleReady()
-} else {
-  app.loadDatabase(() => {
-    app.handleReady()
-  })
-}
+(async () => {
+  if (!config.logToDatabase) {
+    logger.log('warn', 'Database logging is not enabled.');
+    // handleReady에 콜백을 추가하여 서버가 준비되면 로그를 남깁니다.
+    app.handleReady(() => {
+      logger.info('Application is running and listening for connections (DB logging disabled).');
+    });
+  } else {
+    await app.loadDatabase();
+    // handleReady에 콜백을 추가하여 서버가 준비되면 로그를 남깁니다.
+    app.handleReady(() => {
+      logger.info('Application is running and listening for connections.');
+    });
+  }
+})();
