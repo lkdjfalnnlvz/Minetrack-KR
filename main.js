59aca5bf226333d8144ff32fe0cc227d973a8c32
lkdjfalnnlvz <155760188+lkdjfalnnlvz@users.noreply.github.com>
2025-10-19 14:02:03 +0900

    Refactor main.js to use async main function

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-const App = require('./lib/app');
-const ServerRegistration = require('./lib/servers');
-const logger = require('./lib/logger');
-const config = require('./config');
-const servers = require('./servers');
+const App = require('./lib/app')
+const ServerRegistration = require('./lib/servers')
+const logger = require('./lib/logger')
+const config = require('./config')
+const servers = require('./servers')
 
-const app = new App();
+const main = async () => {
+  const app = new App()
 
-servers.forEach((server, serverId) => {
-  if (!server.color) {
-    let hash = 0;
-    for (let i = server.name.length - 1; i >= 0; i--) {
-      hash = server.name.charCodeAt(i) + ((hash << 5) - hash);
+  if (config.logToDatabase) {
+    try {
+      await app.loadDatabase()
+    } catch (err) {
+      logger.error('Failed to initialize database. Exiting.', err)
+      process.exit(1)
     }
-    const color = Math.floor(Math.abs((Math.sin(hash) * 10000) % 1 * 16777216)).toString(16);
-    server.color = '#' + Array(6 - color.length + 1).join('0') + color;
+  } else {
+    logger.warn('Database logging is not enabled.')
   }
-  app.serverRegistrations.push(new ServerRegistration(app, serverId, server));
-});
 
-if (!config.serverGraphDuration) {
-  logger.log('warn', '"serverGraphDuration" is not defined in config.json - defaulting to 3 minutes!');
-  config.serverGraphDuration = 3 * 60 * 10000;
+  servers.forEach((server, serverId) => {
+    if (!server.color) {
+      let hash = 0
+      for (let i = server.name.length - 1; i >= 0; i--) {
+        hash = server.name.charCodeAt(i) + ((hash << 5) - hash)
+      }
+      const color = Math.floor(Math.abs((Math.sin(hash) * 10000) % 1 * 16777216)).toString(16)
+      server.color = '#' + Array(6 - color.length + 1).join('0') + color
+    }
+    app.serverRegistrations.push(new ServerRegistration(app, serverId, server))
+  })
+
+  if (!config.serverGraphDuration) {
+    logger.warn('"serverGraphDuration" is not defined in config.json - defaulting to 3 minutes!')
+    config.serverGraphDuration = 3 * 60 * 10000
+  }
+
+  const port = process.env.PORT || config.site.port
+  config.site.port = parseInt(port, 10)
+
+  app.handleReady()
 }
 
-(async () => {
-  if (!config.logToDatabase) {
-    logger.log('warn', 'Database logging is not enabled.');
-    // handleReady에 콜백을 추가하여 서버가 준비되면 로그를 남깁니다.
-    app.handleReady(() => {
-      logger.info('Application is running and listening for connections (DB logging disabled).');
-    });
-  } else {
-    await app.loadDatabase();
-    // handleReady에 콜백을 추가하여 서버가 준비되면 로그를 남깁니다.
-    app.handleReady(() => {
-      logger.info('Application is running and listening for connections.');
-    });
-  }
-})();
+main()
